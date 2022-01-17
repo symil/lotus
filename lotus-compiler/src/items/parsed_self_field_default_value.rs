368f04eee83a8bbe5f877f1007764bb492344fcc
Sylvain Milan <milan.sylvain@gmail.com>
2022-01-17 22:26:46 +0100

    Add completion for default values declaration.

--- lotus-compiler/src/items/parsed_self_field_default_value.rs
+++ lotus-compiler/src/items/parsed_self_field_default_value.rs
@@ -1,6 +1,6 @@
 use parsable::parsable;
 use crate::program::{ProgramContext, Type, Vasm};
-use super::{ParsedSelfKeyword, ParsedDotToken, Identifier, ParsedEqualToken, ParsedExpression, unwrap_item, ParsedCommaToken};
+use super::{ParsedSelfKeyword, ParsedDotToken, Identifier, ParsedEqualToken, ParsedExpression, unwrap_item, ParsedCommaToken, ParsedSemicolonToken};
 
 #[parsable(cascade = true)]
 pub struct ParsedSuperFieldDefaultValue {
@@ -10,13 +10,19 @@ pub struct ParsedSuperFieldDefaultValue {
     pub equal: Option<ParsedEqualToken>,
     pub expression: Option<ParsedExpression>,
     #[parsable(cascade = false)]
-    pub comma: Option<ParsedCommaToken>,
+    pub semicolon: Option<ParsedSemicolonToken>,
 }
 
 impl ParsedSuperFieldDefaultValue {
     pub fn process(&self, self_type: &Type, context: &mut ProgramContext) -> Option<(String, Vasm)> {
         let dot = unwrap_item(&self.dot, self, context)?;
+
+        context.completion_provider.add_field_completion(dot, self_type, false, false);
+
         let name = unwrap_item(&self.name, self, context)?;
+
+        context.completion_provider.add_field_completion(name, self_type, false, false);
+
         let equal = unwrap_item(&self.equal, self, context)?;
         let expression = unwrap_item(&self.expression, self, context)?;
         let field_type = match self_type.get_field(name.as_str()) {
@@ -36,6 +42,8 @@ impl ParsedSuperFieldDefaultValue {
             },
             None => return None,
         };
+
+        unwrap_item(&self.semicolon, self, context);
 
         Some((name.to_string(), vasm))
     }
