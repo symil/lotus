a73f43f45ad74129df659e02558b1babe35409e5
Sylvain Milan <milan.sylvain@gmail.com>
2022-08-06 00:48:10 +0200

    Update package versions.

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+cd $SCRIPT_DIR/lotus-compiler
+npm install
+cargo build --release
 sudo ln -sf $SCRIPT_DIR/scripts/run-cli.js /usr/local/bin/lotus-cli
 sudo chmod +x /usr/local/bin/lotus-cli
+cd - > /dev/null
