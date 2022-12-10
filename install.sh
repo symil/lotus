acfdb256ad31f6e03845377da649e21275ae5d1a
Sylvain Milan <milan.sylvain@gmail.com>
2022-12-10 23:59:53 +0100

    Build directly with outpost.

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ set -e
 
 cd $SCRIPT_DIR/lotus-compiler
 npm install
-rustup default nightly
 cargo build --release
 sudo ln -sf $SCRIPT_DIR/scripts/run-cli.js /usr/local/bin/lotus-cli
 sudo chmod +x /usr/local/bin/lotus-cli
